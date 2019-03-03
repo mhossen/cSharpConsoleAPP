091fc36344efd75b94fb2072bce2fc400cf56cd6
Mohammed Hossen <mhossen@wgu.edu>
2019-03-02 19:36:12 -0500

    Added calculator and program

---
+++ ConsoleApplicationForlearning/Calculate.cs
@@ -0,0 +1,50 @@
+﻿namespace ConsoleApplicationForlearning
+{
+    public class Calculate
+    {
+        public int Add(int firstValue, int secondValue)
+        {
+            return firstValue + secondValue;
+        }
+
+        public double Add(double firstValue, double secondValue)
+        {
+            return firstValue + secondValue;
+        }
+
+        public int Sub(int firstValue, int secondValue)
+        {
+            return firstValue - secondValue;
+        }
+
+        public double Sub(double firstValue, double secondValue)
+        {
+            return firstValue - secondValue;
+        }
+
+        public int Multi(int firstValue, int secondValue)
+        {
+            return firstValue * secondValue;
+        }
+
+        public double Multi(double firstValue, double secondValue)
+        {
+            return firstValue * secondValue;
+        }
+
+
+        public int Devide(int firstValue, int secondValue)
+        {
+            if (secondValue != 0)
+                return firstValue / secondValue;
+            return firstValue;
+        }
+
+        public double Devide(double firstValue, double secondValue)
+        {
+            if (secondValue != 0.00D)
+                return firstValue / secondValue;
+            return firstValue;
+        }
+    }
+}
--- ConsoleApplicationForlearning/Program.cs
+++ ConsoleApplicationForlearning/Program.cs
@@ -1,15 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApplicationForlearning
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+
+        static string operators = "+ - * /";
+        public static void Main(string[] args)
         {
+            Calculate calc = new Calculate();
+            Console.WriteLine("Enter First Number...");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Second Number...");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Select an Operator: {operators}");
+            string operations = Console.ReadLine();
+
+            switch (operations)
+            {
+                case "+":
+                    Console.WriteLine($"Sum of two number is: {calc.Add(firstNumber, secondNumber)}");
+                    break;
+                case "-":
+                    Console.WriteLine($"Difference of two number is: {calc.Sub(firstNumber, secondNumber)}");
+                    break;
+                case "*":
+                    Console.WriteLine($"Multiplied of two number is: {calc.Multi(firstNumber, secondNumber)}");
+                    break;
+                case "/":
+                    Console.WriteLine($"Division of two number is: {calc.Devide(firstNumber, secondNumber)}");
+                    break;
+                default:
+                    Console.WriteLine("Something went wrong");
+                    break;
+            }
+
+
+            Console.ReadKey();
         }
+
     }
 }
